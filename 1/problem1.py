c52286d28c2a2ab33e8c03376dee85dfc631eb72
Ricardo Ruiz <riki319@gmail.com>
2016-09-03 17:17:17 +0200

    Simplest first problem solution so far

--- 1/problem1.py
+++ 1/problem1.py
@@ -1,31 +1,14 @@
 # -*- coding: utf-8 -*-
 # author: Ricardo Ruiz
 
-is_divisible = lambda a,b: (a % b) == 0
-
-def is_multiple(a, *numbers):
-	"""Return true if a is multiple of at last one of the numbers tuple"""
-
-	for num in numbers:
-		if is_divisible(a, num):
-			return True
-
 
 def sum_all_multiples(of=[3,5], below=1000):
-	"""Return the sum of all the multiples of *of* below *below"""
+	"""Return the sum of all the multiples of=[3,5], below=[1000]"""
 
-	naturals_below = range(1, below)
-	multiples = [num for num in naturals_below if is_multiple(num, *of)]
-	
-	return sum(multiples)
-
-def main():
-
-	result = sum_all_multiples()
-	print(result)
-
+	return sum(num for num in range(1, below) if any(not num % div for div in of))
 
 
 if __name__ == '__main__':
 
-	main()
+	print(sum_all_multiples())
+
