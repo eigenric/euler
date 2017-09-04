989cf0f6e57de3b1f30b1a6f4f264b71d18f2a3a
Ricardo Ruiz <riki319@gmail.com>
2017-09-04 15:08:28 +0200

    PEP8 euler001.py

--- euler001.py
+++ euler001.py
@@ -10,13 +10,12 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def sum_all_multiples(of=[3,5], below=1000):
-	"""Return the sum of all the multiples of=[3,5], below=[1000]"""
 
-	return sum(num for num in range(1, below) if any(not num % div for div in of))
+def sum_all_multiples(of=[3, 5], below=1000):
+    """Return the sum of all the multiples of=[3,5], below=[1000]"""
+    return sum(num for num in range(1, below)
+               if any(not num % div for div in of))
 
 
 if __name__ == '__main__':
-
-	print(sum_all_multiples())
-
+    print(sum_all_multiples())
