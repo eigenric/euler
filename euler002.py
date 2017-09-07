0890deb5f38295de896a72e384b6afd88c1eb57e
Ricardo Ruiz <riki319@gmail.com>
2017-09-07 15:47:19 +0200

    PEP8 euler002.py

--- euler002.py
+++ euler002.py
@@ -15,25 +15,27 @@ exceed four million.
 
 from math import sqrt
 
-phi = ( 1 + sqrt(5) ) / 2.0
+
+phi = (1 + sqrt(5)) / 2.0
+
 
 def fibonacci(n):
-	"""Explicit formula for the n position
-	of fibonnaci sequence"""
+    """Explicit formula for the n position
+    of fibonnaci sequence"""
 
-	result = int((phi**n - (phi-sqrt(5))**n) / sqrt(5))
+    result = int((phi**n - (phi-sqrt(5))**n) / sqrt(5))
+    return result
 
-	return result
 
 def sum_all_fib_even_numbers(limit=4000000):
-	"""Because the sum of two odd numbers is even,
-	even numbers are in [3, 6, 9..] positions"""
+    """Because the sum of two odd numbers is even,
+    even numbers are in [3, 6, 9..] positions"""
 
-	evens = range(3, 100, 3)
+    evens = range(3, 100, 3)
 
-	return sum([fibonacci(even) for even in evens if fibonacci(even) <= limit])
+    return sum([fibonacci(even) for even in evens if fibonacci(even) <= limit])
 
 if __name__ == '__main__':
 
-	solution = sum_all_fib_even_numbers()
-	print(solution)
+    solution = sum_all_fib_even_numbers()
+    print(solution)
