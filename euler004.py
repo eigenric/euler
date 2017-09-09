b43c4d8a22bd8db14d3454e3a3552e8c40149dea
Ricardo Ruiz <pwaqostao@gmail.com>
2017-09-09 20:46:58 +0200

    PEP8 euler004.py

--- euler004.py
+++ euler004.py
@@ -12,23 +12,25 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 import itertools
 
+
 def is_palindrome(number):
 
-	return str(number)[::-1] == str(number)
+    return str(number)[::-1] == str(number)
+
 
 def largest_palindrome_product(digits):
 
-	possible_products = itertools.product(range(1, 10**digits), repeat=2)
+    possible_products = itertools.product(range(1, 10**digits), repeat=2)
 
-	largest = 0
-	for a, b in possible_products:
-		product = a * b
+    largest = 0
+    for a, b in possible_products:
+            product = a * b
 
-		if is_palindrome(product) and product > largest:
-			largest = product
+            if is_palindrome(product) and product > largest:
+                    largest = product
 
-	return largest
+    return largest
 
 if __name__ == '__main__':
 
-	print(largest_palindrome_product(3))
+    print(largest_palindrome_product(3))
